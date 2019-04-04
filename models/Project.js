befdc6d20fbb63f933be91adfa61c7b209403fad
Tuan Le <tuanleminh1235@gmail.com>
2019-04-03 21:52:44 -0400

    Add timestamp

--- models/Project.js
+++ models/Project.js
@@ -1,54 +1,59 @@
 const mongoose = require("mongoose")
 
-const ProjectSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    developers: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-            required: true
-        }
-    ],
-    views: {
-        type: Number,
-        default: 0
-    },
-    stars: {
-        type: Number,
-        default: 0
-    },
-    categories: [
-        {
+const ProjectSchema = new mongoose.Schema(
+    {
+        name: {
             type: String,
             required: true
-        }
-    ],
-    languages: [
-        {
+        },
+        description: {
             type: String,
             required: true
-        }
-    ],
-    photos: [
-        {
-            type: String,
-            required: true
-        }
-    ],
-    externals: [String],
-    reviews: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Review"
-        }
-    ]
-})
+        },
+        developers: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User",
+                required: true
+            }
+        ],
+        views: {
+            type: Number,
+            default: 0
+        },
+        stars: {
+            type: Number,
+            default: 0
+        },
+        categories: [
+            {
+                type: String,
+                required: true
+            }
+        ],
+        languages: [
+            {
+                type: String,
+                required: true
+            }
+        ],
+        photos: [
+            {
+                type: String,
+                required: true
+            }
+        ],
+        externals: [String],
+        reviews: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Review"
+            }
+        ]
+    },
+    {
+        timestamps: true
+    }
+)
 
 module.exports = mongoose.model("Project", ProjectSchema)
