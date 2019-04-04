d2ac75d89486434098ede403ff694cd0ae19e189
Tuan Le <tuanleminh1235@gmail.com>
2019-04-04 12:01:36 -0400

    Add scroll event

--- client/src/App/Navbar.js
+++ client/src/App/Navbar.js
@@ -2,11 +2,37 @@ import React, { Component } from "react"
 import Logo from "../static/Logo.png"
 
 class Navbar extends Component {
+    state = {
+        shadow: false
+    }
+    componentDidMount() {
+        window.addEventListener("scroll", this.handleScroll)
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.handleScroll)
+    }
+
+    handleScroll = () => {
+        if (window.pageYOffset > 10) {
+            if (!this.state.shadow)
+                this.setState({
+                    shadow: true
+                })
+        } else
+            this.setState({
+                shadow: false
+            })
+    }
+
     render() {
         return (
             <nav
                 className="navbar sticky-top navbar-expand-lg navbar-light"
-                style={{ backgroundColor: "white" }}>
+                style={{
+                    backgroundColor: "white",
+                    boxShadow: this.state.shadow ? "0px 1px 10px #555" : ""
+                }}>
                 <div className="container">
                     <a className="navbar-brand" href="/">
                         <img style={{ width: 60 }} src={Logo} alt="Logo" />
--- client/src/Homepage/index.js
+++ client/src/Homepage/index.js
@@ -43,6 +43,96 @@ class Homepage extends Component {
                         </div>
                     </div>
                 </div>
+
+                <div className="container row">
+                    <ul className="col-md-5 list-group">
+                        <li className="list-group-item d-flex justify-content-between align-items-center active">
+                            Cras justo odio
+                            <span className="badge badge-primary badge-pill">
+                                x
+                            </span>
+                        </li>
+                        <li className="list-group-item d-flex justify-content-between align-items-center">
+                            Dapibus ac facilisis in
+                        </li>
+                        <li className="list-group-item d-flex justify-content-between align-items-center">
+                            Morbi leo risus
+                        </li>
+                    </ul>
+                    <div className="col-md-7">
+                        <div>
+                            <h5>Featured Projects</h5>
+                            <dl className="list-inline dl-horizontal">
+                                <li
+                                    className="list-inline-item card"
+                                    style={{ width: "18em" }}>
+                                    <img
+                                        src="https://dummyimage.com/600x400"
+                                        className="card-img-top"
+                                        alt="Project Image"
+                                    />
+                                    <div className="card-body">
+                                        <h5 className="card-title">
+                                            Project #0
+                                        </h5>
+                                        <p className="card-text">Lorem ipsum</p>
+                                    </div>
+                                </li>
+                                <li
+                                    className="list-inline-item card"
+                                    style={{ width: "18em" }}>
+                                    <img
+                                        src="https://dummyimage.com/600x400"
+                                        className="card-img-top"
+                                        alt="Project Image"
+                                    />
+                                    <div className="card-body">
+                                        <h5 className="card-title">
+                                            Project #0
+                                        </h5>
+                                        <p className="card-text">Lorem ipsum</p>
+                                    </div>
+                                </li>
+                                <li
+                                    className="list-inline-item card"
+                                    style={{ width: "18em" }}>
+                                    <img
+                                        src="https://dummyimage.com/600x400"
+                                        className="card-img-top"
+                                        alt="Project Image"
+                                    />
+                                    <div className="card-body">
+                                        <h5 className="card-title">
+                                            Project #0
+                                        </h5>
+                                        <p className="card-text">Lorem ipsum</p>
+                                    </div>
+                                </li>
+                                <li
+                                    className="list-inline-item card"
+                                    style={{ width: "18em" }}>
+                                    <img
+                                        src="https://dummyimage.com/600x400"
+                                        className="card-img-top"
+                                        alt="Project Image"
+                                    />
+                                    <div className="card-body">
+                                        <h5 className="card-title">
+                                            Project #0
+                                        </h5>
+                                        <p className="card-text">Lorem ipsum</p>
+                                    </div>
+                                </li>
+                            </dl>
+                        </div>
+                        <div>
+                            <h5>Trending Projects</h5>
+                        </div>
+                        <div>
+                            <h5>Recent Projects</h5>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
