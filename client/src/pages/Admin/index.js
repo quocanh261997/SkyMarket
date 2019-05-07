2a6f782580b325a480f52a47ea308f16c9604493
quocanh261997 <quocanh261997@gmail.com>
2019-05-07 00:37:34 -0400

    Nothing

--- client/src/pages/Admin/index.js
+++ client/src/pages/Admin/index.js
@@ -1,77 +1,65 @@
-import React, { Component } from "react"
-import api from "../../libs/api"
-import ProjectItem from "../components/ProjectItem"
+import React, { Component } from "react";
+import api from "../../libs/api";
+import ProjectItem from "../components/ProjectItem";
 
 class Admin extends Component {
-    state = {
-        id: null,
-        username: "",
-        email: "",
-        photo: "",
-        projects: [...Array(6)]
-    }
+  state = {
+    id: null,
+    username: "",
+    email: "",
+    photo: "",
+    projects: [...Array(6)]
+  };
 
-    componentDidMount() {
-        this.getProjects()
-    }
+  componentDidMount() {
+    this.getProjects();
+  }
 
-    //Get all the uploaded projects
-    getProjects = () => {
-        let id = this.props.match.params.id
-        this.setState({ id })
-        api("get", "/admin").then(({ projects }) => {
-            this.setState({ projects })
-        })
-    }
+  //Get all the uploaded projects
+  getProjects = () => {
+    let id = this.props.match.params.id;
+    this.setState({ id });
+    api("get", "/projects/admin").then(({ projects }) => {
+      this.setState({ projects });
+    });
+  };
 
-    //Decide if a project is to be accepted or rejected
-    decideProject = (id, confirmed) => {
-        api("put", `/admin/${id}`, {
-            confirmed: confirmed
-        }).then(() => {
-            this.setState(prevState => ({
-                projects: prevState.projects.filter(
-                    project => project._id !== id
-                )
-            }))
-        })
-    }
+  //Decide if a project is to be accepted or rejected
+  decideProject = (id, confirmed) => {
+    api("put", `/projects/admin`, {
+      confirmed: confirmed
+    }).then(() => {
+      this.setState(prevState => ({
+        projects: prevState.projects.filter(project => project._id !== id)
+      }));
+    });
+  };
 
-    render() {
-        return (
-            <div className="container">
-                <div className="row">
-                    {this.state.projects.map((p, i) => (
-                        <div>
-                            <ProjectItem
-                                key={p ? p._id : i}
-                                project={p}
-                                size="lage"
-                            />
-                            <button
-                                className="btn btn-success"
-                                onClick={this.decideProject(
-                                    p ? p._id : i,
-                                    true
-                                )}
-                            >
-                                Accept
-                            </button>
-                            <button
-                                className="btn btn-danger"
-                                onClick={this.decideProject(
-                                    p ? p._id : i,
-                                    false
-                                )}
-                            >
-                                Reject
-                            </button>
-                        </div>
-                    ))}
-                </div>
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          {this.state.projects.map((p, i) => (
+            <div>
+              <ProjectItem key={p ? p._id : i} project={p} size="lage" />
+              <button
+                className="btn btn-success"
+                onClick={() => this.decideProject(p ? p._id : i, true)}
+              >
+                Accept
+              </button>
+              <button
+                className="btn btn-danger"
+                onClick={() => this.decideProject(p ? p._id : i, false)}
+              >
+                Reject
+              </button>
             </div>
-        )
-    }
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Admin
+export default Admin;
