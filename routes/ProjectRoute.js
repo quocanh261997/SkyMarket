6e0a9c5c58da0d90b28c5b87a698b830099adf9e
Michael Nguyen <quocanh261997@gmail.com>
2021-11-25 09:17:54 +0800

    Add script to generate mock data

--- routes/ProjectRoute.js
+++ routes/ProjectRoute.js
@@ -1,6 +1,9 @@
 const db = require("../models"),
     jwt = require("jsonwebtoken"),
-    router = require("express").Router()
+    router = require("express").Router(),
+    dotenv = require("dotenv")
+
+dotenv.config()
 
 const authorize = (req, _, next) => {
     try {
